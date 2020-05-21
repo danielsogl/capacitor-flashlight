b90149e535f98e52b60615eb39e94c3d9f17d027
Daniel Sogl <mytechde@outlook.com>
2020-05-21 20:18:45 +0200

    feat(ios): add bare ios functions

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,5 +4,8 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(FlashlightPlugin, "FlashlightPlugin",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isAvailable, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(switchOn, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(switchOff, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isSwitchedOn, CAPPluginReturnPromise);
 )
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -8,10 +8,24 @@ import Capacitor
 @objc(FlashlightPlugin)
 public class FlashlightPlugin: CAPPlugin {
     
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+    @objc func isAvailable(_ call: CAPPluginCall) {
         call.success([
-            "value": value
+            "value": true
+        ])
+    }
+    
+    @objc func switchOn(_ call: CAPPluginCall) {
+        let intensity = call.getDouble("intensity") ?? 1.0
+        call.success()
+    }
+    
+    @objc func switchOff(_ call: CAPPluginCall) {
+        call.success()
+    }
+    
+    @objc func isSwitchedOn(_ call: CAPPluginCall) {
+        call.success([
+            "value": true
         ])
     }
 }
