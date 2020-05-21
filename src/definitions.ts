3bb983b4c2a10139abe066f20f778ce5c376627f
Daniel Sogl <mytechde@outlook.com>
2020-05-21 20:10:33 +0200

    feat(plugin): add plugin interfaces

--- src/definitions.ts
+++ src/definitions.ts
@@ -6,5 +6,28 @@ declare module "@capacitor/core" {
 }
 
 export interface FlashlightPluginPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  /**
+   * Checks if flashlight is available
+   * @returns  {Promise<{ value: boolean }>} true if flashlight is available and false if not
+   */
+  isAvailable(): Promise<{ value: boolean }>;
+
+  /**
+   * Turns the flashlight on
+   * @returns {Promise<void>} an empty Promise
+   * @param intensity The intensity of the light between 0.0 and 1.0
+   */
+  switchOn(intensity?: number): Promise<void>;
+
+  /**
+   * Turns the flashlight off
+   * @rejects PluginResultError
+   */
+  switchOff(): Promise<void>;
+
+  /**
+   * Checks if the flashlight is turned on or off
+   * @returns  {Promise<{ value: boolean }>} True of flaslight is turned on and false if not
+   */
+  isSwitchedOn(): Promise<{ value: boolean }>;
 }
